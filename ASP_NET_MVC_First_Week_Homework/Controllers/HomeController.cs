e3bdaef4b7b15fa6613854140c87c66cfd1209df
jayhanlin <jayhan0905@hotmail.com>
2019-12-02 23:38:49 +0800

    week1_homework

--- ASP_NET_MVC_First_Week_Homework/Controllers/HomeController.cs
+++ ASP_NET_MVC_First_Week_Homework/Controllers/HomeController.cs
@@ -31,10 +31,26 @@ namespace ASP_NET_MVC_First_Week_Homework.Controllers
         public ActionResult Homework()
         {
             //TODO
-            //隨機產生在這
-            var qwe = new daily_money_logViewModel();
-            qwe.SerialNumber = 
-            return View();
+            //隨機產生在這           
+            //List SNList= new List<SerialNumber>
+            var result = new List<daily_money_logViewModel>();
+            for (int i = 1; i <= 101; i++)
+            {
+                
+                result.Add()
+                result.SerialNumber = i % 20;
+                result.type = "good";
+                result.date = DateTime.Now;
+                result.Amount = 100;
+            }
+            //result.date = DateTime.Now;
+            //result.SerialNumber = 5;
+            //var random = new Random(Guid.NewGuid().GetHashCode());
+            //result.type = random.NextDouble() > 0.5 ? "支出" : "收入";
+            //result.Amount = 5000;
+        
+
+            return View(result);
         }
     }
 }
